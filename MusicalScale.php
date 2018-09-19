01c01e39cf2444d6fa6576619d0e9cd3d83ad0b5
Unknown <EBoblett949@west-mec.org>
2018-09-19 15:26:17 -0700

    started coding
    started coding

---
+++ MusicalScale.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html>
+<head>
+    <!-- 
+        Exercise 02_03_01
+        Author: Eli Boblett
+        Date: 9.19.18 
+        MusicalScale.php
+     -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Musical Scale</title>
+    <script src="modernizr.custom.65897.js"></script>
+</head>
+<body>
+<h2>Musical Scale</h2>
+    <?php
+        $musicalScale = array("do", "re", "me", "fa", "so", "la", "ti", "do");
+        $outputString = "The notes of the musical scale are: ";
+    ?>
+</body>
+</html>
