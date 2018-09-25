f3be7b2641aac6a73299ec5bce3ca4de34f7518a
Unknown <EBoblett949@west-mec.org>
2018-09-25 15:05:49 -0700

    final

---
+++ random stuff/yeah.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html>
+<head>
+    <!-- 
+        random stuff
+        Author: Eli Boblett
+        Date: 9.12.18 
+         yeah.php
+     -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Button page</title>
+    <script src="modernizr.custom.65897.js"></script>
+</head>
+<body>
+<h2>Search Wiki 2</h2>
+<textarea name="search" id="search" cols="15" rows="1"></textarea>
+    <?php
+        echo "<input type=\"button\" value=\"Search\" onclick=\"location='yeet.php'\" />";
+     ?>
+</body>
+</html> 
---
+++ random stuff/yeet.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html>
+<head>
+    <!-- 
+        random stuff
+        Author: Eli Boblett
+        Date: 9.12.18 
+         yeah.php
+     -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bout to finna</title>
+    <script src="modernizr.custom.65897.js"></script>
+</head>
+<body>
+<h2></h2>
+    <?php
+        echo "<h1>This page is unavailable</h1>";
+     ?>
+</body>
+</html> 
